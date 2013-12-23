e37215843e4787309b92627ff63d25b8f8cf0d50
chinna-muhil <chinna_muhil@yahoo.com>
2013-12-23 22:49:47 +0530

    save, print,email option are implemented.

--- public/javascripts/SearchPageScript.js
+++ public/javascripts/SearchPageScript.js
@@ -1,5 +1,6 @@
 function showListView(){
     $('#MapDiv2').remove();
+    var selectedImage;
     document.getElementById('mainAside').innerHTML='<div id=MapDiv>' +
                                                         '<ul id="searchList">'+
                                                             "<li class=zoomedDetails style=width:100%><div id=propertyZoomedDetails><br />" +
@@ -10,12 +11,12 @@ function showListView(){
                                                             +"<img src="+propertyList[0].picture.linkRight+" width=580 height=270/>"
                                                             +"</div></div>"+
                                                             "<div id=description style=width:102.5%><div id=tabs>" +
-                                                            "<ul><li><img src=/images/maps_button.png >" +
-                                                            "</li><li><img src=/images/messgae.png >" +
-                                                            "</li><li><img src=/images/save_button.png >" +
-                                                            "</li><li><img src=/images/some_button.png >" +
-                                                            "</li><li><img src=/images/print.png >" +
-                                                            "</li></ul>" +
+                                                            "<ul><li><button id='mapimg'><img src=/images/maps_button.png  title='Go To Location In Map'>" +
+                                                            "</button></li><li><button id='messageimg'><img src=/images/messgae.png  title='Email Selected Image'>" +
+                                                            "</button></li><li><button id='saveimg'><img src=/images/save_button.png  title='Save Selected Image' >" +
+                                                            "</button></li><li><button id='socialimg'><img src=/images/some_button.png  title='Share Selected Image'>" +
+                                                            "</button></li><li><button id='printimg'><img src=/images/print.png title='Print Selected Image'>" +
+                                                            "</button></li></ul>" +
                                                             "</div><div id=price><p style=padding-left:0>Price: $"+propertyList[0].price+"</p>" +
                                                             "</div><div id=property><div id=property_details><h4>Property Details</h4></div> " +
                                                             "<div id=property_types><ul><li id=beds>Beds: "+propertyList[0].bedrooms+"</li>" +
@@ -105,8 +106,38 @@ function showListView(){
                                                                 "<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.</p></div>" +
                                                                 "</div></div></li>");   */
                                                             //li.insertAfter($("ul#searchList li:first"));
+    
+        var url = $(location).attr('href');
+        url = url.substring(0, url.indexOf('/search'));
+        
+        
+        $('#socialimg').click(function(){
+        var a = $('<a>').attr('href', url+'/fbauth').attr('target', '_blank').appendTo('body');
+        a[0].click();
+        a.remove();
+        });                                    
+
+
+        $('#messageimg').click(function(){
+            var a = $("<a>").attr("href", url+'/sendmail'+selectedImage).appendTo("body");
+            a[0].click();
+            a.remove();
+          });                                    
+        $('#printimg').click(function(){
+            var printMe = window.open(url+selectedImage, 'Print'); 
+            printMe.print();
+          });
+
+        $('#saveimg').click( function() {
+            var a = $("<a>").attr("href", url+selectedImage).attr("download", selectedImage).appendTo("body");
+            a[0].click();
+            a.remove();
+        });
+
+        
         $("#propertyPrice").live('click',function() {
             //alert("Yayy");
+            console.log("propertyPrice Ready...");
             var i = $(this).attr('class');
             console.log(i);
             $("#price p").html("Price: $"+propertyList[i].price);
@@ -128,6 +159,7 @@ function showListView(){
                 speed:1000,
                 infiniteLoop:true,
                 buildPager: function(slideIndex){
+                    console.log("bxslider Ready...");
                     switch(slideIndex){
                         case 0:
                             return '<img src='+propertyList[i].picture.linkFront+' width=100 height=50>';
@@ -139,12 +171,14 @@ function showListView(){
                             return '<img src='+propertyList[i].picture.linkRight+' width=100 height=50>';
                     }
                 }
+
             });
 
             return false;
         });
-
+        
         $(document).ready(function(){
+            console.log("Document Ready...");
             $('.bxslider').bxSlider({
                 mode: 'horizontal',
                 auto: true,
@@ -152,6 +186,7 @@ function showListView(){
                 speed:1000,
                 infiniteLoop:true,
                 buildPager: function(slideIndex){
+                    console.log("Document Ready..."+slideIndex);
                     switch(slideIndex){
                         case 0:
                             return '<img src='+propertyList[0].picture.linkFront+' width=100 height=50>';
@@ -162,7 +197,26 @@ function showListView(){
                         case 3:
                             return '<img src='+propertyList[0].picture.linkRight+' width=100 height=50>';
                     }
-                }
+                },
+                 onSliderLoad: function(currentIndex){
+                  console.log('Slider has finished loading. Click OK to continue!');
+          },
+          onSlideAfter: function($slideElement, oldIndex, newIndex){
+                    switch(newIndex){
+                        case 0:
+                            selectedImage = propertyList[0].picture.linkFront;
+                            break;
+                        case 1:
+                            selectedImage = propertyList[0].picture.linkBack;
+                            break;
+                        case 2:
+                            selectedImage = propertyList[0].picture.linkLeft;
+                            break;
+                        case 3:
+                            selectedImage = propertyList[0].picture.linkRight;
+                            break;
+                    }
+          }
             });
         })
 
